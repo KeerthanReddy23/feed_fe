90296553f78fa6c7ad68a988c75a8d999b2b39ff
Keerthan Reddy <keerthan0000@gmail.com>
2024-09-09 00:56:03 -0700

    Update MainLayout.js

--- src/Pages/MainLayout.js
+++ src/Pages/MainLayout.js
@@ -3,31 +3,34 @@ import { jwtDecode } from 'jwt-decode';
 import { useRecoilState } from 'recoil';
 import { userState } from '../Recoil/atoms';
 import SideNavbar from '../components/SideNavbar'
+import { useNavigate } from 'react-router-dom';
 
 const MainLayout = () => {
 
-  const [user, setUser] = useRecoilState(userState);
-
+  const [, setUser] = useRecoilState(userState);
+  const navigate = useNavigate();
+  
   useEffect(() => {
-    if (!user){
-      const accessToken = localStorage.getItem('access');
-      if (accessToken) {
-        try {
-          const decoded = jwtDecode(accessToken);
-          const userInfo = {
-            userid: decoded.user.userid,
-            firstname: decoded.user.first_name,
-            lastname: decoded.user.last_name,
-            email: decoded.user.email,
-          };
-          setUser(userInfo);
-        } catch (error) {
-          setUser(null);
-          console.error('Failed to decode JWT token', error);
-        }
+    const accessToken = localStorage.getItem('access');
+    if (accessToken) {
+      try {
+        const decoded = jwtDecode(accessToken);
+        const userInfo = {
+          userid: decoded.user.userid,
+          firstname: decoded.user.first_name,
+          lastname: decoded.user.last_name,
+          email: decoded.user.email,
+        };
+        setUser(userInfo);
+      } catch (error) {
+        setUser(null);
+        console.error('Failed to decode JWT token', error);
       }
     }
-  }, [user, setUser]);
+    else{
+      navigate('/login');
+    }
+  }, [setUser, navigate]);
 
   return (
     <SideNavbar/>
